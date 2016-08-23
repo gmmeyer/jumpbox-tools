3c9a224e17ea3fabd33e59fec95d14a777635a1c
Greg Meyer <greg.meyer@datadoghq.com>
2016-08-23 13:36:12 -0400

    oh my zsh without git

--- installs.sh
+++ installs.sh
@@ -1,8 +1,13 @@
 # ZSH install
-if [ ! -d ~/.oh-my-zsh ]; then
-    echo "Installing OMZ"
-    curl -L http://install.ohmyz.sh | zsh
+if command -v git >/dev/null 2>&1; then
+  if [ ! -d ~/.oh-my-zsh ]; then
+      echo "Installing OMZ"
+      curl -L http://install.ohmyz.sh | zsh
+  else
+      echo "OMZ already installed"
+  fi
 else
-    echo "OMZ already installed"
+  ./oh-my-zsh.sh
 fi
+
 rm -f ~/.zshrc.pre-oh-my-zsh

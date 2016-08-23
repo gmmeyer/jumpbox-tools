e9342d2c6ca454ba9b55623d601e35d685ab52de
Greg Meyer <greg.meyer@datadoghq.com>
2016-08-23 13:27:40 -0400

    allows installation on systems without git

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,17 @@ rm -rf .bash* .bin* .cache* .chef* .config* .gitconfig .lesshst .oh-my-zsh .prof
 # In case we are re-running it
 rm -rf .jumpbox-tools
 
-# Clone ourselves
-git clone https://github.com/gmmeyer/jumpbox-tools.git ~/.jumpbox-tools
+# Clone ourselves, or download the tarball if there isn't git.
+if command -v git >/dev/null 2>&1; then
+    git clone https://github.com/gmmeyer/jumpbox-tools.git ~/.jumpbox-tools
+else
+    curl -L https://api.github.com/repos/gmmeyer/jumpbox-tools/tarball > jumpbox-tools.tar.gz
+    tar -xzf jumpbox-tools.tar.gz
+    DIR=$(ls | grep gmmeyer-jumpbox-tools)
+    mv $DIR ~/.jumpbox-tools
+    rm jumpbox-tools.tar.gz
+fi
+
 cd ~/.jumpbox-tools
 
 ./installs.sh

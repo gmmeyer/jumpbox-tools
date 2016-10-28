6ed6544c5ed1eeaa9c9bcaa170737a189efad153
Greg Meyer <greg.meyer@datadoghq.com>
2016-10-28 14:09:36 -0400

    direct link to gh script rather than to ohmyz.sh

--- installs.sh
+++ installs.sh
@@ -2,7 +2,7 @@
 if command -v git >/dev/null 2>&1; then
   if [ ! -d ~/.oh-my-zsh ]; then
       echo "Installing OMZ"
-      curl -L http://install.ohmyz.sh | zsh
+      curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | zsh
   else
       echo "OMZ already installed"
   fi

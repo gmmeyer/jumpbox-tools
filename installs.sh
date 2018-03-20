08755dd1ea942a6921c0cb5b1927bfbe60c36f22
Greg Meyer <greg.meyer@datadoghq.com>
2018-03-20 11:17:44 -0400

    updates

--- installs.sh
+++ installs.sh
@@ -2,7 +2,7 @@
 if command -v git >/dev/null 2>&1; then
   if [ ! -d ~/.oh-my-zsh ]; then
       echo "Installing OMZ"
-      curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | zsh
+      sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   else
       echo "OMZ already installed"
   fi
